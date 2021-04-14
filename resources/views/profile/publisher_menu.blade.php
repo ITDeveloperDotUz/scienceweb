9d390217ff164ce3b611c9b899353032620d4ddf
Abror Aliboyev <48922487+ITDeveloperDotUz@users.noreply.github.com>
2021-04-14 17:33:10 +0500

    fixed config error

---
+++ resources/views/profile/publisher_menu.blade.php
@@ -0,0 +1,53 @@
+
+{{--<div class="row gutters-sm">--}}
+{{--    <div class="col-6 text-center">--}}
+{{--        <a href="{{ route('event.create') }}">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-body">--}}
+{{--                    <h5 class="mb-1"><strong>Collect</strong></h5>--}}
+{{--                    <i class="fa fa-bullhorn fa-2x"></i>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </a>--}}
+{{--    </div>--}}
+{{--    <div class="col-6 text-center">--}}
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h5 class="mb-1"><strong>Submissions</strong></h5>--}}
+{{--                <i class="fa fa-envelope fa-2x"></i>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-6 text-center mt-3">--}}
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h5 class="mb-1"><strong>Archive</strong></h5>--}}
+{{--                <i class="fa fa-archive fa-2x"></i>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-6 text-center mt-3">--}}
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h5 class="mb-1"><strong>Authors</strong></h5>--}}
+{{--                <i class="fa fa-group fa-2x"></i>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-6 text-center mt-3">--}}
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h5 class="mb-1"><strong>Metrics</strong></h5>--}}
+{{--                <i class="fa fa-bar-chart-o fa-2x"></i>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-6 text-center mt-3">--}}
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h5 class="mb-1"><strong>Settings</strong></h5>--}}
+{{--                <i class="fa fa-gears fa-2x"></i>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
